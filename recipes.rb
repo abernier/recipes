49e39271ed0fc63baa5d7c48118981b7f2c69f5f
Antoine BERNIER <antoine.bernier@gmail.com>
2010-02-22 18:09:08 +0100

    Initial commit.

---
+++ recipes.rb
@@ -0,0 +1,137 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'dm-core'
+require 'dm-validations'
+
+#
+# Model
+#
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/recipes.db")
+
+class Recipe
+  include DataMapper::Resource
+  
+  property :id,           Serial
+  property :title,        String
+  property :slug,         String
+  
+  has n, :authors, :through => Resource, :model => 'User'
+end
+
+class User
+  include DataMapper::Resource
+  
+  property :id,         Serial
+  property :email,      String, :format => :email_address
+  property :name,       String
+  
+  has n, :recipes, :through => Resource
+end
+DataMapper.auto_upgrade!
+
+#
+# Controller
+#
+
+# List
+get '/' do
+  haml :list, :locals => { :rs => Recipe.all }
+end
+
+# Show
+get '/:id' do |id|
+  haml :show, :locals => { :r => Recipe.get(id) }
+end
+
+# New / Create
+get '/new' do
+  haml :form, :locals => {
+    :r => Recipe.new,
+    :action => '/create'
+  }
+end
+
+post '/create' do
+  r = Recipe.new
+  r.attributes = params
+  r.save
+
+  redirect("/#{r.id}")
+end
+
+# Edit / Update
+get '/:id/edit' do |id|
+  c = Contact.get(id)
+  haml :form, :locals => {
+    :r => r,
+    :action => "/#{r.id}/update"
+  }
+end
+
+post '/:id/update' do |id|
+  r = Recipe.get(id)
+  r.update_attributes params
+
+  redirect "/#{id}"
+end
+
+# Delete
+post '/:id/delete' do |id|
+  r = Recipe.get(id)
+  r.destroy
+
+  redirect "/"
+end
+
+#
+# View
+#
+
+__END__
+@@ layout
+%html
+  %head
+    %title Recipes
+  %body
+    = yield
+    %a(href="/") Recipes list
+
+@@form
+%h1 Add a new recipe
+%form(id="add-recipe" action="#{action}" method="POST")
+  %label(for="title") Title
+  %input(type="text" id="title" name="title" value="#{r.title}")
+  %br
+  
+  %label(for="slug") Slug
+  %input(type="text" id="slug" name="slug" value="#{r.slug}")
+  %br
+
+  %input(type="submit")
+  %input(type="reset")
+
+- unless r.id == 0
+  %form(action="/#{r.id}/delete" method="POST")
+    %input(type="submit" value="Delete")
+  
+@@show
+%table
+  %tr
+    %td Title
+    %td= r.title
+%a(href="/#{r.id}/edit") Edit recipe
+
+@@list
+%h1 Recipes
+%a(href="/new") New recipe
+%table
+  - rs.each do |r|
+    %tr
+      %td= r.title
+      %td
+        %a(href="/#{r.id}") Show
+      %td
+        %a(href="/#{r.id}/edit") Edit
